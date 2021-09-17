da9ebe151128aa5f6d817e6431150f16b9b5b03e
abdalllah22 <ahmed.abdalllah22@gmail.com>
2021-09-17 13:03:52 +0200

    add dealing with CSV files in parser

--- parser.py
+++ parser.py
@@ -2,7 +2,8 @@ import sys
 
 from utils.xml_parser import XMLParser
 from utils.helper import ( get_format_of_file, get_first_file_name,
-                            get_second_file_name, select_all_files )
+                            get_second_file_name, select_all_files,
+                            select_all_CSV_files )
 
 
 
@@ -17,8 +18,11 @@ def main():
                 print('File does not exis')
         
         elif file_format == 'csv':
-            pass
-        
+            files = select_all_CSV_files()
+            if get_first_file_name(sys.argv[2]) and get_first_file_name(sys.argv[3])  in files:
+                print('done')
+            else:
+                print('File does not exis')
             
     except Exception as e:
         print(e)
